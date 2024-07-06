858f2658c46f55ee983bac983f395cdc362b9b55
大田辰郎 <ta26on@ootatatsurounoMac-mini.local>
2024-07-06 11:44:14 +0900

    add function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function readAttendanceSubject(ss)
-{
-  const sheet = ss.getSheetByName( 'フォームの回答 2' )
+function readAttendanceSubject(ss) {
+  const sheet = ss.getSheetByName('フォームの回答 2')
 
   // This represents ALL the data
   var range = sheet.getDataRange();
@@ -14,55 +13,51 @@ function readAttendanceSubject(ss)
     row['type'] = values[i][1]
     row['title'] = values[i][2]
     row['date'] = values[i][3]
-    row['deadline'] = values[i][4] == null ? '': values[i][4]
+    row['deadline'] = values[i][4] == null ? '' : values[i][4]
     ret.push(row)
   }
 
-  ret.sort((a,b)=>{ a['date'] < b['date'] ? 1 : -1})
+  ret.sort((a, b) => { a['date'] < b['date'] ? 1 : -1 })
 
   return ret
 }
 
-function isExistTitle(sheet, title, date)
-{
+function isExistTitle(sheet, title, date) {
   values = sheet.getRange('b1:j2').getValues()
-  for (let col=0; col < values[0].length; col++)
-  {
+  for (let col = 0; col < values[0].length; col++) {
     _title = values[0][col]
     _date = values[1][col]
 
-    if (title != _title){
+    if (title != _title) {
       continue
     }
-    if(date.toDateString() != _date.toDateString())
-    {
+    if (date.toDateString() != _date.toDateString()) {
       continue
     }
-      return true
-  } 
+    return true
+  }
   return false
 }
 
 function addColumn() {
-    const ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1PSS_v3A5h0srfkSJXeteHQbpXFkpZYDIMlkuuJk3sCM/edit')
+  const ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1PSS_v3A5h0srfkSJXeteHQbpXFkpZYDIMlkuuJk3sCM/edit')
 
-    const subjects = readAttendanceSubject(ss)
+  const subjects = readAttendanceSubject(ss)
 
-    const parts = ['鳴り物', '女踊り', '男踊り(青)', '女男踊り(助)','浴衣','子供(黄)']
-    //const parts = ['鳴り物']
+  const parts = ['鳴り物', '女踊り', '男踊り(青)', '女男踊り(助)', '浴衣', '子供(黄)']
+  //const parts = ['鳴り物']
 
-    subjects.forEach((subject) => 
-    {
-      parts.forEach((part) => {
-        addColumnToSheet(ss.getSheetByName(part), subject)
-      })
+  subjects.forEach((subject) => {
+    parts.forEach((part) => {
+      addColumnToSheet(ss.getSheetByName(part), subject)
     })
+  })
 }
 
 function addColumnToSheet(sheet, subject) {
   const title = `${subject['type']} ${subject['title']}`
 
-  if (isExistTitle(sheet, title, subject['date'])==true){return}
+  if (isExistTitle(sheet, title, subject['date']) == true) { return }
 
   sheet.insertColumnBefore(2)
 
@@ -74,6 +69,11 @@ function addColumnToSheet(sheet, subject) {
 function showAttendances() {
   const sheet = SpreadsheetApp.getActiveSheet()
   const col = sheet.getActiveRange().getColumn()
+  let msg = makeMgsAttendancesOnePart(sheet, col)
+  Browser.msgBox(msg)
+}
+
+function makeMgsAttendancesOnePart(sheet, col) {
   let msg = sheet.getRange(1, col).getValue() + "\\n"
 
   const num_rows = sheet.getMaxRows()
@@ -89,7 +89,7 @@ function showAttendances() {
       const answer = sheet.getRange(row, col).getValue()
       if (answer.includes(type)) {
         const member_name = sheet.getRange(row, 1).getValue()
-        attendances[type].push(`${member_name} ${answer.replace(type,'')}`)
+        attendances[type].push(`${member_name} ${answer.replace(type, '')}`)
       }
     }
     msg += `${type}: ${attendances[type].length}\\n`
@@ -97,14 +97,13 @@ function showAttendances() {
       msg += row + "\\n"
     })
   })
-
-  Browser.msgBox(msg)
+  return msg
 }
 
 function onOpen() {
   const customMenu = SpreadsheetApp.getUi()
-  customMenu.createMenu('しるばあ機能')  
-      .addItem('列追加', 'addColumn')
-      .addItem('集計', 'showAttendances')
-      .addToUi()
+  customMenu.createMenu('しるばあ機能')
+    .addItem('列追加', 'addColumn')
+    .addItem('集計', 'showAttendances')
+    .addToUi()
 }
